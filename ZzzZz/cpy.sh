47c989a95a07e93b8402bdf21db6f53b0c57679b
Adam Jacobsson <adam.jacobsson@users-MacBook-Air.local>
2021-03-12 21:43:29 +0100

    woops

---
+++ ZzzZz/cpy.sh
@@ -0,0 +1,14 @@
+cpy() {
+	if [ -z "$1" ]
+	then
+    	echo "Project name needs to be an argument \"cpy <project_name>\""
+    	return 0
+	fi
+
+	mkdir -p  $1/source
+	cp ~/conf/pyright/pyrightconfig.json $1
+	cd $1/source
+	touch app.py
+
+	venv
+}
