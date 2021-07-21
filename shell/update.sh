a974ffdcbd66c82814690893c14148965517b74e
Adam Jacobsson <adam.jacobsson@users-MacBook-Air.local>
2021-07-22 00:46:22 +0200

    gaah

--- shell/update.sh
+++ shell/update.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+cd "$(dirname "$0")"
 git remote update > /dev/null
 
 UPSTREAM=${1:-'@{u}'}
@@ -8,10 +9,12 @@ REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 
 if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
+    :
 elif [ $LOCAL = $BASE ]; then
     vared -p "adamame is behind, do you wish to pull latest? [y/n]: " -c yn
     if [ "$yn" = "y" ]; then
         git pull
     fi
 fi
+
+cd ~
